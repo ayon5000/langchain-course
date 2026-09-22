dc57b56ff41f2aa7421dfa733e7493042792f446
ayon5000 <ayon5000@gmail.com>
2026-09-22 09:52:16 +0530

    using langchain tavily search tool

--- search_agent/main.py
+++ search_agent/main.py
@@ -3,35 +3,22 @@ from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
 from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 _ = load_dotenv()
 
-tavily = TavilyClient()
 
-@tool
-def search_tool(query: str) -> str:
-    """
-    Tool that searches over the internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
 
-    print(f"Searching for {query}")
-
-    return tavily.search(query=query)
 
 llm = ChatOpenAI(temperature=0, model="gpt-5")
-tools = [search_tool]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 def main():
 
 
     result = agent.invoke({
-        "messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the Kolkata on linkedin and list their details?")
+        "messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in Prague on linkedin and list their details?")
     })
 
     print(result)
